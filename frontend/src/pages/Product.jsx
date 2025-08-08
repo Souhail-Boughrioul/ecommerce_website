2f3691b4426bb1a23c63284feb972e73f92da599
Souhail <souhailboughrioul@gmail.com>
2025-08-09 00:26:27 +0100

    The product page is done and i create the cart page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,10 +2,11 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 const Product = () => {
   const {productId} = useParams();
-  const {products, currency} = useContext(ShopContext);
+  const {products, currency, addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size, setSize] = useState(false);
@@ -15,7 +16,6 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item)
         setImage(item.image[0])
-        console.log(item)
         return null;
       }
     })
@@ -67,8 +67,31 @@ const Product = () => {
                   ))}
                 </div>
               </div>
+
+              <button onClick={()=> addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+              <hr className='mt-8 sm:w-4/5'/>
+              <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                  <p>100% Original product.</p>
+                  <p>Cash on delivery is available on this product.</p>
+                  <p>Easy return and exchange policy within 7 days.</p>
+              </div>
           </div>
         </div>
+
+        {/* Description & Review Section */}
+        <div className='mt-20'>
+          <div className='flex'>
+            <b className='border px-5 py-3 text-sm'>Description</b>
+            <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+          </div>
+          <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+            <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
+            <p>E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant information.</p>
+          </div>
+        </div>
+
+        {/* Related Products */}
+        <RelatedProduct category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div>
 
